a6bbe6fbe5e825925da1fc3c05b469d5f4790707
Brendan Dileo <dileob23@gmail.com>
2026-02-20 22:48:27 -0500

    Add GBA save size and struct definition to header

--- include/pkmnemerald.h
+++ include/pkmnemerald.h
@@ -1,4 +1,9 @@
 
+/**
+ * pkmnemerald.h
+ * Author: Brendan Dileo
+ */
+
 #ifndef PKMNEMERALD_H
 #define PKMNEMERALD_H
 
@@ -6,8 +11,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+#define SAVE_SIZE 131072            // 128kb - GBA save size
+
 typedef struct {
+    uint8_t data[SAVE_SIZE];        // Raw bytes of the save file
+    size_t size;                    // Number of bytes read
+    int is_valid;                   // Whether the save file loaded correctly
 } SaveFile;
+
+
+SaveFile* load_save(const char *filepath);
 
 
 #endif 
